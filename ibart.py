d909a966952242db2f1cc52d21f86fc5e0ad7d73
Joakim Bech <joakim.bech@linaro.org>
2021-08-20 13:50:39 +0200

    authentication: only allow command when authenticated
    Added support to authenticate users via Google login. When logged in
    it's possible to manually restart and stop builds. People how aren't
    logged in will get no active links and if they try to access the links
    directly they will get an "unauthorized" page.
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- ibart.py
+++ ibart.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort, session
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 import hashlib
 import hmac
 import json
 import logging as log
 import os
+import pathlib
+import requests
 import sys
 
 # Local imports
@@ -14,6 +20,31 @@ import logger as ibl
 import worker
 
 app = Flask(__name__)
+
+# This signs the cookies
+app.secret_key = os.environ['APP_SECRET_KEY']
+
+# For development using no SSL, uncomment this.
+# os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "1094173588005-032jr02th379346mn9rqetbigbmac9pp.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://optee.mooo.com:5000/callback"
+)
+
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return
 
 
 def verify_hmac_hash(data, signature):
@@ -42,8 +73,12 @@ def dump_json_blob_to_file(request, filename="last_blob.json"):
 @app.route('/')
 def main_page(page=1):
     sql_data = db.get_html_row(page)
-    return render_template('main.html', sd=sql_data, page=page)
 
+    google_id = None
+    if "google_id" in session:
+        google_id = session["google_id"]
+
+    return render_template('main.html', sd=sql_data, page=page, google_id=google_id)
 
 @app.route('/<int:page>')
 def main_paginate(page):
@@ -53,6 +88,9 @@ def main_paginate(page):
 
 @app.route('/restart/<int:pr_id>/<pr_sha1>')
 def restart_page(pr_id, pr_sha1):
+    if "google_id" not in session:
+        abort(401)
+
     worker.user_add(pr_id, pr_sha1)
     # if request.is_secure:
     #    if request.referrer:
@@ -62,6 +100,9 @@ def restart_page(pr_id, pr_sha1):
 
 @app.route('/stop/<int:pr_id>/<pr_sha1>')
 def stop_page(pr_id, pr_sha1):
+    if "google_id" not in session:
+        abort(401)
+
     worker.cancel(pr_id, pr_sha1)
     # if request.is_secure:
     #    if request.referrer:
@@ -123,6 +164,41 @@ def payload():
 
         worker.add(payload)
     return 'OK'
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 
 @app.errorhandler(404)
